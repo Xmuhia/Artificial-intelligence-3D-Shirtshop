9b11133438009d43ab235789eb473c122120810b
Brendan Muhia <brendanmuhia@Brendans-MacBook-Pro.local>
2023-11-29 03:00:13 +0300

    Updating Vite Config File

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    entry: 'client/index.html',
+    outDir: 'dist', 
   },
+  resolve: {
+    alias: {
+      '@': '/src', 
+    },
+  },
+  main: 'src/main.jsx', 
 });
