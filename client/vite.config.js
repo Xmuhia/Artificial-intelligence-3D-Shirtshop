5f85bb9614afcda46d09d9054655c345f9ff914c
Brendan Muhia <dotcombrendan@gmail.com>
2023-12-29 21:22:05 +0300

    Fixing Deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: '/src/index.html',
+      input: '/src/main.jsx', // Pointing to the main JavaScript file
     },
   },
   resolve: {
@@ -14,5 +14,4 @@ export default defineConfig({
       '@': '/src',
     },
   },
-  main: 'src/main.jsx',
 });
